ed237fe8061493962cabb3ebbc9296f89211d5d8
amigo-js <120602850+amigo-js@users.noreply.github.com>
2023-05-15 22:23:00 +0200

    Add the solution of JS home task 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img
+     class="gallery__image"
+     src="${preview}"
+     data-source="${original}"
+     alt="${description}"
+     />
+     </a>
+     </li>`
+  )
+  .join("");
+
+const ulRef = document.querySelector(".gallery");
+ulRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+ulRef.addEventListener("click", imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  } else {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600"> `);
+    instance.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+     <img
+     class="gallery__image"
+     src="${preview}"
+     data-source="${original}"
+     alt="${description}"
+     />
+     </a>
+     </li>`
+  )
+  .join("");
+
+const ulRef = document.querySelector(".gallery");
+ulRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
